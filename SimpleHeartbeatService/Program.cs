4b3add2ea44a2f7262f7d5a8234b053b3eb1b8ea
svetlanavrabie <laurasdamir@gmail.com>
2020-03-12 22:07:29 -0400

    Initial Commit

---
+++ SimpleHeartbeatService/Program.cs
@@ -0,0 +1,31 @@
+﻿using SimpleHeartbeatService.Service;
+using System;
+using Topshelf;
+
+namespace SimpleHeartbeatService
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Heartbeat>(s =>
+                {
+                    s.ConstructUsing(hearbeat => new Heartbeat());
+                    s.WhenStarted(hearbeat => hearbeat.Start());
+                    s.WhenStopped(hearbeat => hearbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("HeartbeatService");
+                x.SetDisplayName("Heartbeat Service");
+                x.SetDescription("this is sample heartbeat service used in....");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ SimpleHeartbeatService/Service/Heartbeat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace SimpleHeartbeatService.Service
+{
+    public class Heartbeat
+    {
+        private readonly Timer _timer;
+
+        public Heartbeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Logs\Heartbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
